11dd5eba02b4de3041df1e866b8d0311bd225661
vijexa <vijexa@DESKTOP-ETMRL27.localdomain>
2020-11-08 23:49:18 +0200

    implemented component that encapsulates chart and selectors

---
+++ src/components/ChartBlock.tsx
@@ -0,0 +1,92 @@
+import * as React from 'react'
+import * as E from 'fp-ts/Either'
+import {pipe} from 'fp-ts/function'
+import styled, { css } from 'styled-components'
+import { CountryDropdown } from 'react-country-region-selector'
+
+import {processData} from '../scripts/processData'
+import fetchRawData from '../scripts/fetchRawData'
+import {DataType, Indicator} from '../types/DataType'
+import CovidChart from './CovidChart';
+import IndicatorDropdown from './IndicatorDropdown'
+import LoadingSpinner from './LoadingSpinner'
+import NoDataMessage from './NoDataMessage'
+
+const DropdownStyles = css`
+  padding: 0.5em;
+  margin-bottom: 0.5em;
+  font-size: 1em;
+  width: 100%;
+  border-radius: 0.5em;
+  appearance: none;
+  background-image: url(down-arrow.jpg) no-repeat right;
+`
+
+const StyledCountryDropdown = styled(CountryDropdown)`
+  ${DropdownStyles}
+`
+
+const StyledIndicatorDropdown = styled(IndicatorDropdown)`
+  ${DropdownStyles}
+`
+
+type ChartBlockProps = {
+  height: number
+}
+ 
+type ChartBlockState = {
+  data?: DataType
+  country?: string
+  indicator: Indicator
+
+  loading: boolean
+}
+ 
+export default class ChartBlock extends React.Component<ChartBlockProps, ChartBlockState> {
+  changeData = (data?: DataType, country?: string, indicator?: Indicator) => 
+    this.setState({
+      data: data, 
+      country: country, 
+      indicator: indicator ?? this.state.indicator, 
+      loading: false
+    })
+
+  constructor(props: ChartBlockProps) {
+    super(props)
+    this.state = {loading: false, indicator: 'confirmed cases'}
+  }
+
+  componentDidMount() {
+    (async () => {
+      this.setState({loading: true})
+      const rawData = await fetchRawData()
+      console.log('rawData: ', rawData)
+      pipe(
+        rawData,
+        E.fold(
+          error   => console.log('error: ', error.error),
+          rawData => this.changeData(processData(rawData))
+        )
+      )
+    })()
+  }
+
+  render() { 
+    const state = this.state
+    const props = this.props
+    return ( 
+      <div>
+        <StyledCountryDropdown value={state.country ?? ''} onChange={(country) => this.changeData(state.data, country, state.indicator) }/>
+        <StyledIndicatorDropdown onChange={(indicator) => this.changeData(state.data, state.country, indicator)} />
+        {
+          state.loading
+            ? <LoadingSpinner height={props.height} />
+            : (state.data && state.country && state.indicator)
+              ? <CovidChart height={props.height} data={state.data} country={state.country} indicator={state.indicator} />
+              : <NoDataMessage height={props.height} country={state.country} indicator={state.indicator} />
+        }
+      </div>
+      
+    )
+  }
+}
