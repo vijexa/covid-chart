9f0bf0fd77ea3c5359b55177ed0b86ce66544b6f
vijexa <nuzhdinvitja@gmail.com>
2020-11-08 23:45:59 +0200

    Added loading spinner animation

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import PulseLoader from 'react-spinners/PulseLoader'
+
+import AlignedContainer from './AlignedContainer'
+
+type LoadingSpinnerProps = {
+  height: number
+}
+ 
+export default class LoadingSpinner extends React.Component<LoadingSpinnerProps> {
+  render() { 
+    const props = this.props
+    return (
+      <AlignedContainer height={props.height}>
+        <PulseLoader size='2em' />
+      </AlignedContainer>
+    );
+  }
+}
