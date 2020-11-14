1c921cc394a4919742a38989a67264c992f374b4
vijexa <nuzhdinvitja@gmail.com>
2020-11-14 03:22:12 +0200

    implemented data format algorithm to show multiple lines

--- src/components/CovidChart.tsx
+++ src/components/CovidChart.tsx
@@ -7,53 +7,91 @@ import {getCountryData, getIndicatorData, sortDataByDate} from '../scripts/proce
 
 type CovidChartProps = {
   data: DataType
-  country: string
-  indicator: Indicator
+  countries: string[]
+  indicators: Indicator[]
   height: number
 }
 
 type ChartRecord = {
-  date: string;
-  rate14day: number;
+  date: string
+  parameterName: string
+  [key: string]: any
 }
 
 export default class CovidChart extends React.Component <CovidChartProps> {
   
-  formatDataOrUndefined = (props: CovidChartProps) => {
-    if (props.data && props.country && props.indicator) {
-      const dataForCountry = getCountryData(props.country, props.data)
-      const dataForIndicator = getIndicatorData(props.indicator, dataForCountry) 
-      console.log(props.country + ", " + props.indicator + ": ", dataForIndicator)
-      const sortedData = sortDataByDate(dataForIndicator)
-      const formattedData: ChartRecord[] = sortedData.map(
-        r => ({
-          date: r.date.getDate() + "." + (r.date.getMonth() + 1), 
-          rate14day: r.rate14day
-        })
-      )
+  // making cringe format data to satisfy rechart
+  formatData = (props: CovidChartProps): ChartRecord[] => {
+    // TODO: optimise
+    const countriesData = props.countries.flatMap(
+      country =>
+        getCountryData(country, props.data)
+    )
 
-      return formattedData
-    } else {
-      return undefined
-    }
+    const indicatorData = props.indicators.flatMap(
+      indicator => 
+        getIndicatorData(indicator, countriesData)
+    )
+
+    // data that is sorted by date and has a property with dynamic name
+    // e.g. 'Afghanistandeathsrate14day'
+    const sortedNamedData: ChartRecord[] = sortDataByDate(indicatorData).map(
+      r => {
+        const parName = r.country + r.indicator + 'rate14day'
+        return {
+          date: r.date.getDate() + "." + (r.date.getMonth() + 1), 
+          parameterName: parName,
+          [parName]: r.rate14day
+        }
+      }
+    )
+
+    // data that is formatted in a chart-friendly format where dynamic properties
+    // from sortedNameData are grouped by date
+    const formattedData: ChartRecord[] = sortedNamedData.reduce(
+      (acc, curr, i, arr) => {
+        if (arr[i - 1]?.date === curr.date) {
+          acc[acc.length - 1][curr.parameterName] = curr[curr.parameterName]
+          return acc
+        } else {
+          return [...acc, curr]
+        }
+      },
+      [] as ChartRecord[]
+    )
+
+    console.log("datasets:", props.countries, ", ", props.indicators, ": ", formattedData)
+
+    return formattedData
   }
 
   render() {
     const props = this.props
     
-    const formattedData = this.formatDataOrUndefined(props)
+    const formattedData = this.formatData(props)
+    const objectData = Object.fromEntries(Object.entries(formattedData))
 
     console.log("props: ", props, ", formatted data: ", formattedData)
+    console.log("objectData", objectData)
 
     return (
       <div>
-        Chart for {props.indicator} in {props.country}, 14 day cumulative
+        Chart for {props.indicators.join(' and ')} in {props.countries.join(', ')}, 14 day cumulative
         <ResponsiveContainer width="100%" height={props.height}>
           <LineChart style={{fontSize: 20}} data={formattedData}>
-            <Line type="monotone" dataKey="rate14day" stroke="#8884d8" dot={false} strokeWidth={3} />
             <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
             <XAxis dataKey="date" interval={30} dx={10} height={40} tick={<AngledTick angle={-35} />} />
             <YAxis />
+
+            {
+              props.countries.flatMap(
+                country => props.indicators.map(
+                  indicator => 
+                    <Line key={country + indicator} type="monotone" dataKey={country + indicator + 'rate14day'} stroke="#8884d8" dot={false} strokeWidth={3} />
+                )
+              )
+            }
+            
           </LineChart>
         </ResponsiveContainer>
       </div>
