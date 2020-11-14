0efe062d2ecaf253a22ecccd43b76bac9fe5dcf6
vijexa <nuzhdinvitja@gmail.com>
2020-11-14 03:17:13 +0200

    improved country dropdown for multiple selection

---
+++ src/components/CountryDropdown.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react'
+import MultiSelect from "react-multi-select-component"
+
+import {Indicator} from '../types/DataType'
+
+
+export type OptionType = {
+  value: string
+  label: string
+}
+
+interface CountryDropdownProps  {
+  className?: string
+  values: string[]
+  options: OptionType[]
+
+  onChange: (country: string[]) => void
+}
+ 
+export default class CountryDropdown extends React.Component<CountryDropdownProps> {
+
+
+  render() { 
+    const props = this.props
+
+    return (
+      <MultiSelect 
+        className={props.className} 
+        value={props.options.filter(option => props.values.includes(option.value))} 
+        options={props.options} 
+        onChange={(selected: OptionType[]) => props.onChange(selected.map(v => v.value))} 
+        labelledBy="Select country"
+        hasSelectAll={false}
+      />
+    )
+  }
+}
+ 
