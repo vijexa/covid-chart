34abba4db84be07b08237dd5bf34db8775f219ad
vijexa <nuzhdinvitja@gmail.com>
2020-11-15 03:52:40 +0200

    fixed title and scrollbar

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,13 @@ import ChartBlock from './components/ChartBlock'
 const StyledApp = styled.div`
   text-align: left;
   background-color: #ffffff;
-  min-height: 95vh;
   display: flex;
   flex-direction: column;
   align-items: left;
   justify-content: flex-start;
   font-size: calc(10px + 2vmin);
   color: rgb(39, 39, 39);
-  padding: 5%;
+  padding: 1em;
 `
 
 const StyledHeader = styled.header`
@@ -25,7 +24,7 @@ function App() {
   return (
     <StyledApp>
       <StyledHeader>
-        Super cool app description
+        Covid Chart
       </StyledHeader>
       <ChartBlock chartHeight={500} />
     </StyledApp>
