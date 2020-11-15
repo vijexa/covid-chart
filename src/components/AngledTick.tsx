5cecd2ec8a7815b0279905da7440233a530f4636
vijexa <nuzhdinvitja@gmail.com>
2020-11-16 01:15:42 +0200

    added initial margin to x axis ticks

--- src/components/AngledTick.tsx
+++ src/components/AngledTick.tsx
@@ -1,14 +1,32 @@
 import React from 'react'
 
-export default class AngledTick extends React.PureComponent<any> {
+type AngledTickProps = {
+  x?: number
+  y?: number
+  payload?: any
+
+  angle: number
+  initMargin?: number
+}
+
+export default class AngledTick extends React.PureComponent<AngledTickProps> {
     render() {
       const {
         x, y, payload,
       } = this.props;
   
       return (
-        <g transform={`translate(${x},${y})`}>
-          <text x={0} y={0} dy={16} textAnchor="end" fill="#666" transform={`rotate(${this.props.angle})`}>{payload.value}</text>
+        <g transform={`translate(${(x ?? 0) + (this.props.initMargin ?? 0)},${y})`}>
+          <text 
+            x={0} 
+            y={0} 
+            dy={16} 
+            textAnchor="end" 
+            fill="#666" 
+            transform={`rotate(${this.props.angle})`}
+          >
+            {payload.value}
+          </text>
         </g>
       );
     }
--- src/components/CovidChart.tsx
+++ src/components/CovidChart.tsx
@@ -119,7 +119,7 @@ export default class CovidChart extends React.Component <CovidChartProps> {
         <ResponsiveContainer width="100%" height={props.height}>
           <LineChart style={{fontSize: 20}} data={formattedData}>
             <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-            <XAxis dataKey="date" interval={30} dx={10} height={40} tick={<AngledTick angle={-55} />} />
+            <XAxis dataKey="date" interval={30} dx={10} height={40} tick={<AngledTick angle={-55} initMargin={-4} />} />
             <YAxis width={35}/>
             <Legend />
 
